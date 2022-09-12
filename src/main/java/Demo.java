8adf204371f073701e488ab18b2fce13076291f7
spkto-umar <spk2umar01@gmail.com>
2022-09-12 15:58:05 +0530

    Jenkins first commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<10;i++)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.println("Jenkin workspace git+maven");
+	}
+
+}
